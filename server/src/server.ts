de68529d41a018a3260c5ba65d981508d7e9d95e
muhammad-jiku <muhammadjiku364@gmail.com>
2024-12-30 13:58:21 +0600

    db init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,55 @@
 import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorlogger, logger } from './shared/logger';
 
 process.on('uncaughtException', (error) => {
-  console.error(error);
+  errorlogger.error(error);
+  // console.error(error);
   process.exit(1);
 });
 
 let server: Server;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is connected successfully!`);
+    // console.log(`Database is connected successfully!`);
+
+    server = app.listen(config.port, () => {
+      logger.info(
+        `[server]: Server is running at http://localhost:${config?.port}`
+      );
+      // console.log(
+      //   `[server]: Server is running at http://localhost:${config?.port}`
+      // );
+    });
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
+    // console.error('Failed to connect database', err);
+  }
+
+  process.on('unhandledRejection', (error) => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        // console.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  // console.log('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
